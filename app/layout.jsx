277973eebee74792d04f5f2bed13a8cb410c8b0e
zakariem <zakariyem367@gmail.com>
2024-09-14 08:52:08 +0300

    Add Footer Component and Integrate into Layout
    This commit introduces a new `Footer` component and integrates it into the main layout of the application. The `Footer` component includes the company logo and a copyright notice with the current year dynamically generated. The `Footer` is now displayed at the bottom of the page, enhancing the overall structure and user experience.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "@/assets/styles/globals.css";
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export const metadata = {
@@ -12,6 +13,7 @@ function layout({ children }) {
       <body>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
