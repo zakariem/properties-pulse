8bc3fc13dc9ee98f9658f8c923ffc0a478367f0e
zakariem <zakariyem367@gmail.com>
2024-09-29 08:47:56 +0300

    Refactor fetchProperties function to handle missing API domain
    Previously, the `fetchProperties` function would attempt to fetch data from the API domain specified in the environment variable `NEXT_PUBLIC_API_DOMAIN`. However, if this variable was not set, the function would throw an error.
    
    This commit introduces a check to ensure that the `apiDomain` is not null before making the fetch request. If the `apiDomain` is missing, the function now returns an empty array instead of attempting to fetch data, preventing potential runtime errors.

--- utils/requests.js
+++ utils/requests.js
@@ -1,6 +1,10 @@
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
 export async function fetchProperties() {
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+      if(!apiDomain){
+        return [];
+      }
+      const res = await fetch(`${apiDomain}/properties`);
       if (!res.ok) {
         throw new Error("failed to fetch data");
       }
