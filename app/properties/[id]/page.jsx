79316f3556d9debcbfe3ac5d428038d93903df4f
zakariem <zakariyem367@gmail.com>
2024-10-06 10:47:29 +0300

    Refactor Property Page and Add Spinner Component
    - Simplified the Property component by removing unnecessary conditional rendering and redundant state checks.
    - Added a Spinner component to handle loading states, improving user experience during data fetching.
    - Corrected a typo in the `fetchProperyData` function name to `fetchPropertyData`.
    - Updated the PropertyDetails component to improve the layout of the amenities list.
    - Removed unused icons from the Property component to reduce bundle size.

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -4,149 +4,146 @@ import { useParams } from "next/navigation";
 import { fetchSingleProperty } from "@/utils/requests";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import Link from "next/link";
-import { FaArrowLeft, FaLocationDot, FaBed, FaBath, FaRulerCombined, FaBookmark, FaShare, FaCheck, FaPaperPlane } from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
 import PropertyDetails from "@/components/PropertyDetails";
+import Spinner from "@/components/Spinner";
 
 function Property() {
   const { id } = useParams();
-
   const [property, setProperty] = useState(null);
-  const [loading, setLoading] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchProperyData = async () => {
+    const fetchPropertyData = async () => {
       if (!id) return;
       try {
-        const property = await fetchSingleProperty(id);
-        setProperty(property);
+        const fetchedProperty = await fetchSingleProperty(id);
+        setProperty(fetchedProperty);
       } catch (e) {
-        console.log("====================================");
-        console.error("Error fetching property ", e);
-        console.log("====================================");
+        console.error("Error fetching property", e);
       } finally {
         setLoading(false);
       }
     };
-    if (property === null) fetchProperyData();
-  }, [id, property]);
 
-  if (!property && !loading)
+    fetchPropertyData();
+  }, [id]);
+
+  if (loading) return <Spinner />;
+
+  if (!property)
     return (
-      <h1 classNameName="text-center text-2xl font-bold mt-10">
+      <h1 className="text-center text-2xl font-bold mt-10">
         Property Not Found
       </h1>
     );
 
   return (
     <>
-      {!loading && property && (
-        <>
-          <PropertyHeaderImage image={property.images[0]} />
-          <section>
-            <div className="container m-auto py-6 px-6">
-              <Link
-                href="/properties"
-                className="text-blue-500 hover:text-blue-600 flex items-center"
-              >
-                <FaArrowLeft className="mr-2"></FaArrowLeft> Back to Properties
-              </Link>
-            </div>
-          </section>
-          <section className="bg-blue-50">
-            <div className="container m-auto py-10 px-6">
-              <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-               <PropertyDetails property={property} />
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/properties"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2" /> Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <PropertyDetails property={property} />
 
-                <aside className="space-y-4">
-                  <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-                    <i className="fas fa-bookmark mr-2"></i> Bookmark Property
-                  </button>
-                  <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-                    <i className="fas fa-share mr-2"></i> Share Property
-                  </button>
+            <aside className="space-y-4">
+              <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
+                <i className="fas fa-bookmark mr-2"></i> Bookmark Property
+              </button>
+              <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
+                <i className="fas fa-share mr-2"></i> Share Property
+              </button>
 
-                  <div className="bg-white p-6 rounded-lg shadow-md">
-                    <h3 className="text-xl font-bold mb-6">
-                      Contact Property Manager
-                    </h3>
-                    <form>
-                      <div className="mb-4">
-                        <label
-                          className="block text-gray-700 text-sm font-bold mb-2"
-                          htmlFor="name"
-                        >
-                          Name:
-                        </label>
-                        <input
-                          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                          id="name"
-                          name="name"
-                          type="text"
-                          placeholder="Enter your name"
-                          required
-                        />
-                      </div>
-                      <div className="mb-4">
-                        <label
-                          className="block text-gray-700 text-sm font-bold mb-2"
-                          htmlFor="email"
-                        >
-                          Email:
-                        </label>
-                        <input
-                          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                          id="email"
-                          name="email"
-                          type="email"
-                          placeholder="Enter your email"
-                          required
-                        />
-                      </div>
-                      <div className="mb-4">
-                        <label
-                          className="block text-gray-700 text-sm font-bold mb-2"
-                          htmlFor="phone"
-                        >
-                          Phone:
-                        </label>
-                        <input
-                          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                          id="phone"
-                          name="phone"
-                          type="text"
-                          placeholder="Enter your phone number"
-                        />
-                      </div>
-                      <div className="mb-4">
-                        <label
-                          className="block text-gray-700 text-sm font-bold mb-2"
-                          htmlFor="message"
-                        >
-                          Message:
-                        </label>
-                        <textarea
-                          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 h-44 focus:outline-none focus:shadow-outline"
-                          id="message"
-                          name="message"
-                          placeholder="Enter your message"
-                        ></textarea>
-                      </div>
-                      <div>
-                        <button
-                          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline flex items-center justify-center"
-                          type="submit"
-                        >
-                          <i className="fas fa-paper-plane mr-2"></i> Send Message
-                        </button>
-                      </div>
-                    </form>
+              <div className="bg-white p-6 rounded-lg shadow-md">
+                <h3 className="text-xl font-bold mb-6">
+                  Contact Property Manager
+                </h3>
+                <form>
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="name"
+                    >
+                      Name:
+                    </label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id="name"
+                      name="name"
+                      type="text"
+                      placeholder="Enter your name"
+                      required
+                    />
                   </div>
-                </aside>
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="email"
+                    >
+                      Email:
+                    </label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id="email"
+                      name="email"
+                      type="email"
+                      placeholder="Enter your email"
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="phone"
+                    >
+                      Phone:
+                    </label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id="phone"
+                      name="phone"
+                      type="text"
+                      placeholder="Enter your phone number"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="message"
+                    >
+                      Message:
+                    </label>
+                    <textarea
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 h-44 focus:outline-none focus:shadow-outline"
+                      id="message"
+                      name="message"
+                      placeholder="Enter your message"
+                    ></textarea>
+                  </div>
+                  <div>
+                    <button
+                      className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline flex items-center justify-center"
+                      type="submit"
+                    >
+                      <i className="fas fa-paper-plane mr-2"></i> Send Message
+                    </button>
+                  </div>
+                </form>
               </div>
-            </div>
-          </section>
-        </>
-      )}
+            </aside>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -80,7 +80,7 @@ function PropertyDetails({ property }) {
       <div className="bg-white p-6 rounded-lg shadow-md mt-6">
         <h3 className="text-lg font-bold mb-6">Amenities</h3>
 
-        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none">
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none space-y-2">
           {property.amenities.map((amenity, index) => (
             <li key={index}>
               <FaCheck className="fas fa-check text-green-600 mr-2 mt-3"/>{" "}
